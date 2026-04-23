a5c7497badd1409aadd44570d2dcfe1fe8ed7d24
Ritesh <riteshroyal834047@gmail.com>
2026-04-23 19:42:15 +0530

    Added basic header component

---
+++ disney-clonex/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header style={{ background: "#090b13", padding: "10px" }}>
+      <h2 style={{ color: "white" }}>Disney Clone</h2>
+    </header>
+  );
+}
+
+export default Header;
